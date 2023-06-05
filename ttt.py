f73378cca6f334c326b8c8e9084709711ae0391d
inorilzy <84810566@qq.com>
2023-06-05 19:15:55 +0800

    update

--- ttt.py
+++ ttt.py
@@ -7,7 +7,7 @@ def quick_sort(arr):
   right = [x for x in arr if x > pivot]
   return quick_sort(left) + middle + quick_sort(right)
 
-# python冒泡排序
+# 生成一个 python 函数，功能是从长字符串中找到子字符串，使用kmp算法
 
 def bubble_sort(arr):
   n = len(arr)
@@ -25,6 +25,7 @@ def bubble_sort(arr):
 if __name__ == '__main__':
   arr = [3, 6, 1, 7, 2, 8, 5, 4]
   print(quick_sort(arr))  # 输出  [1,  2,  3,  4,  5,  6,  7,  8]
+
 
 
 
